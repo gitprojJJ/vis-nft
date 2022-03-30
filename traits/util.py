8ebe4f7cc658e4cd1860a5af10da564074c08360
garyleung142857 <garyleung142857@gmail.com>
2022-03-30 10:52:31 +0800

    remove obsolete function

--- traits/util.py
+++ traits/util.py
@@ -9,15 +9,6 @@ def filter_trait(df, trait):
         return df[condition]
 
 
-def set_buckets(df, num_buckets=4):
-    # set price ranges, dividing price ranges into equal number of assets.
-    # Ignoring those without sales
-    # Whole population of the collection
-    prices = df['last_sale_total_price']
-    q = [i / num_buckets for i in range(num_buckets + 1)]
-    return np.nanquantile(prices, q)
-
-
 def find_all_traits(df):
     # return a set of all traits
     all_traits = set()
@@ -27,7 +18,7 @@ def find_all_traits(df):
     return all_traits
 
 
-def find_trait_proportions(df_trait, buckets, proportion=False):
+def find_trait_proportions(df_trait, proportion=False):
     # proportion: as a portion of all (between 0 and 1)
     asset_count = len(df_trait)
     no_sale_count = df_trait['no_sales'].sum()
@@ -46,12 +37,11 @@ def find_trait_proportions(df_trait, buckets, proportion=False):
 def traits_stats(df, trait_names, num_buckets=4):
     # trait_names: list of trait names
     PREC = 4
-    buckets = set_buckets(df, num_buckets)
     all_asset_count = len(df)
     stats = dict()
     for trait_name in trait_names:
         df_trait = filter_trait(df, trait_name)
-        count_t, no_sale_t, freq_t = find_trait_proportions(df_trait, buckets, True)
+        count_t, no_sale_t, freq_t = find_trait_proportions(df_trait, True)
         stats[trait_name] = {
             'rarity': np.round(count_t / all_asset_count, PREC),  # The smaller, the rarer
             'sale_prop': np.round(1 - no_sale_t, PREC),  # proportion of asset (with trait) that have sales
